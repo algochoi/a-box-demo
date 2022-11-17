07d1e88f58443e9d7593f3336a2aade65bc0ed44
algochoi <86622919+algochoi@users.noreply.github.com>
2022-11-17 17:26:32 -0500

    Create some boxes using router

---
+++ box-demo.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import json
+
+from pyteal import (
+    App,
+    Approve,
+    Assert,
+    Balance,
+    BareCallActions,
+    Bytes,
+    CallConfig,
+    Expr,
+    For,
+    Ge,
+    Global,
+    Gt,
+    If,
+    InnerTxnBuilder,
+    Int,
+    Not,
+    OnComplete,
+    OnCompleteAction,
+    Reject,
+    Return,
+    Router,
+    ScratchVar,
+    Seq,
+    Subroutine,
+    TealType,
+    Txn,
+    TxnField,
+    TxnType,
+    abi,
+    pragma,
+)
+
+pragma(compiler_version="^0.20.1")
+
+CONTRACT_VERSION = 8
+
+router = Router(
+    # Name of the contract
+    "box-demo",
+    # What to do for each on-complete type when no arguments are passed (bare call)
+    BareCallActions(
+        no_op=OnCompleteAction.never(),
+    ),
+)
+
+
+@router.method(no_op=CallConfig.CALL)
+def create() -> Expr:
+    """Create a box"""
+    return Seq(
+        # 100 byte box created with box_create
+        Assert(App.box_create(Bytes("MyKey"), Int(100)) == Int(1))
+    )
+
+
+@router.method(no_op=CallConfig.CALL)
+def put() -> Expr:
+    return Seq(
+        # box created with box_put
+        App.box_put(Bytes("MyKey"), Bytes("My Values")),
+        Approve(),
+    )
+
+
+if __name__ == "__main__":
+    approval, clearstate, contract = router.compile_program(version=CONTRACT_VERSION)
+
+    with open("approve-box.teal", "w") as f:
+        f.write(approval)
+
+    with open("clear-box.teal", "w") as f:
+        f.write(clearstate)
